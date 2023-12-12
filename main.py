6e343553d201d80e5c5b80763b01b0f2029b6187
RicardoVasquezzzz <153631005+RicardoVasquezzzz@users.noreply.github.com>
2023-12-12 09:40:57 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+#Suma
+a = 10
+b = 5
+
+#suma de dos numeros enteros usando print
+print("La suma es:", (a+b))
+
+#Resta
+c = 16.5
+d = 5.2
+
+#resta de dos numeros de punto flotante usando print
+print("La resta es:", (c-d))
+
+#Multiplicación
+e = 30
+f = 18
+
+#multiplicaión de dos numeros enteros usando print
+print("La multiplicación es:", (e*f))
+
+#División
+g = 50.8
+h = 10.5
+
+print("La división es:", (g/h))
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#Nombre y Longitud
+
+#Definir el nombre en la variable
+Nombre = "Ricardo Thomas Vasquez"
+#Contar los caracteres con la función len
+long = len(Nombre)
+
+print("Mi nombre es:", Nombre)
+print("Longitud es:", long)
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+#Conversión de datos
+dato1 = 56
+dato2 = 20.5
+
+print("tipo de dato :", type(dato1))
+print("tipo de dato en la variable 2:", type(dato2))
+
+convertir1 = float(dato1)
+convertir2 = int(dato2)
+
+print("El tipo de dato convertir:", type(convertir1))
+print("El tipo de dato convertir2:", type(convertir2))
